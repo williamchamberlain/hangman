d3a176b02d3aea69f0540683f0be68679b809853
williamchamberlain <33006200+williamchamberlain@users.noreply.github.com>
2022-11-12 14:14:44 +0000

    Basic functionality added with collison and word dtetction
    The game will now display the word on the output and if you click the button it will either add to the hangman or fill it in. Just print statement for win/loss curnetly.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
+import math
+import random
 #eqution for buttons      width-(gap+radius*2)*button-on-row/2
+
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
@@ -16,19 +19,13 @@ pygame.display.set_caption("Hangman by Will C")
 
 
 LETTER_FONT = pygame.font.SysFont('comicsans', 40) #font for letters 
+WORD_FONT = pygame.font.SysFont('comicsans', 60)
 
-
-
-
-images = []
-for i in range(7): #load in 6 images (use 7 because it's up to 7)
-  image = pygame.image.load("hangman" +str(i) + ".png") #loads in image (1-6)
-  images.append(image)
-
+#BUTTON VARIABLES
 
 RADIUS = 20 #for button
 GAP = 15
-letters = []
+letters = [] 
 startx = round((WIDTH - (RADIUS * 2 +GAP) * 13) / 2)
 starty = 400
 
@@ -37,9 +34,23 @@ A=65
 for i in range(26): #x and y postion for each button
   x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))  #the percentage sign returns remainder until 13 (the second row)
   y = starty + ((i // 13) * (GAP + RADIUS * 2)) #// is whole number division
-  letters.append([x,y, chr(A+i)]) #chr is character
+  letters.append([x,y, chr(A + i), True]) #chr is character
 
 
+
+
+#LOAD IMAGES
+images = []
+for i in range(7): #load in 6 images (use 7 because it's up to 7)
+  image = pygame.image.load("hangman" +str(i) + ".png") #loads in image (1-6)
+  images.append(image)
+
+
+  
+#GAME VARIABLES 
+hangman_status = 0 #what image is needed
+word="COLLEGE"
+guessed=[""] #what letters the player has guessed 
 
 
 
@@ -50,17 +61,37 @@ for i in range(26): #x and y postion for each button
 def draw(): #for the circle
   win.fill((WHITE)) #fills white
 
+
+  #DRAW LETTERS 
+  display_word = ""
+  for letter in word:
+    if letter in guessed:
+      display_word += letter + " " #word being displayed with space if its in the word
+    else:
+      display_word += "_ "
+
+  text = WORD_FONT.render(display_word, 1 ,BLACK)
+  win.blit(text,(400,200))
+
+    
+
+
+  
+  #DRAW BUTTONS
   for letter in letters:
-    x , y, ltr = letter #splitting up variable
-    pygame.draw.circle(win,BLACK, (x, y), RADIUS, 3)
-    text = LETTER_FONT.render(ltr, 1, BLACK)
-    win.blit(text, (x - text.get_width()/2, y - text.get_height()/2))
+    x , y, ltr, visible = letter #splitting up variable
+    if visible:
+      
+      pygame.draw.circle(win,BLACK, (x, y), RADIUS, 3)
+      text = LETTER_FONT.render(ltr, 1, BLACK)
+      win.blit(text, (x - text.get_width()/2, y - text.get_height()/2))
     
   
   win.blit(images[hangman_status], (150,100) ) #blit is draw
   pygame.display.update() 
 
-hangman_status = 6 #what image is needed
+
+
 
 
 
@@ -85,9 +116,35 @@ while run: #while run is true (playing)
       if event.type == pygame.QUIT: 
           run = False
       if event.type ==pygame.MOUSEBUTTONDOWN:
-        pos = pygame.mouse.get_pos() #pos is postion of where it is clicked
-        print(pos) #note for future self cooridnates are from top left 
+        m_x , m_y = pygame.mouse.get_pos() #gets x and y where it is clicked
+        for letter in letters:
+          x, y , ltr, visible = letter
+          if visible:
+              
+            dis = math.sqrt((x - m_x)**2 + (y-m_y)**2) #pythagoruous
+            if dis < RADIUS:
+              letter[3]= False
+              guessed.append(ltr)
+              if ltr not in word:
+                hangman_status += 1
+          
+  won = True
+
+    
+  for letter in word:
+    if letter not in guessed:
+      won= False
+      break
         
+  if won:
+    print("won")
+    break
+
+  if hangman_status == 6:
+    print("lost")
+    break
+      
+        #if the mouse is less than the radius away from centre there is no collision
         
 
 
